e01bd5d8e5ff820a5df1a7315271024223006b91
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-09-12 14:15:37 -0700

    question validates presence of description

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
 
 has_many :answers
 belongs_to :user
+validates :description, presence: true
 
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,6 @@ describe Question do
   it { should belong_to :user }
 
   it { should have_many :answers }
+
+  it { should validate_presence_of :description}
 end
