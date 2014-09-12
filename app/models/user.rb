85f01ee1694bf03043dbef194de2d0a246172d0f
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-09-12 14:03:50 -0700

    association of user to questions and answers set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :questions
+
+  has_many :answers
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe User do
 
+  it { should have_many :questions }
+
+  it { should have_many :answers }
+
 end
