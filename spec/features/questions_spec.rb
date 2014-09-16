53ad3dd584b3cadc0909ee861e1d3796c00afaee
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-09-16 15:52:02 -0700

    capybara will take input that correctly searches all questions

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -7,9 +7,20 @@ describe "questions all page" do
     q1 = Question.create(description: 'what is a computer', user_id: user.id, favorite_answer_id: answer.id)
     q2 = Question.create(description: 'where is the monitor', user_id: user.id, favorite_answer_id: answer.id)
     visit '/questions'
-    save_and_open_page
     click_link 'what is a computer'
     expect(page).to have_content 'All Answers:'
+  end
+
+  it 'will take input that correctly searches all questions' do
+    user = User.create(name: 'nacho libre', password: 'libre', password_confirmation: 'libre')
+    answer = Answer.create(description: 'short and sweet')
+    q1 = Question.create(description: 'what is a computer', user_id: user.id, favorite_answer_id: answer.id)
+    q2 = Question.create(description: 'where is the monitor', user_id: user.id, favorite_answer_id: answer.id)
+    visit "/questions"
+    find_field('search')
+    fill_in 'search', with: 'computer'
+    click_button 'search'
+    expect(page).to have_content 'what is a computer'
   end
 
 end
