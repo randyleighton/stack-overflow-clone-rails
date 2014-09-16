f4521e785057afb8efe2db1065650fb6c64fbe4a
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-09-16 11:53:29 -0700

    test for logged in user redirecting to his/her show page from nav bar link

--- spec/features/routes_spec.rb
+++ spec/features/routes_spec.rb
@@ -6,5 +6,16 @@ describe 'page visitor viewing all questions' do
     find(:xpath, "//a/img[@alt='Question']/..").click
     expect(page).to have_content "Click on a question"
   end
+end
 
+describe 'logged in viewers can view their show page from the nav bar link' do
+  it 'will redirect a logged in user to his/her show page' do
+    user = User.create(name: 'nacho libre', password: 'libre')
+    visit '/login'
+    fill_in 'Name', with: 'nacho libre'
+    fill_in 'Password', with: 'libre'
+    click_button 'Log In'
+    click_link 'nacho libre'
+    expect(page).to have_content "Questions written by nacho libre"
+  end
 end
